5fea6c7d25a924aa4ab8985263aad10e164b4493
marcomws <marco.mws@outlook.com>
2021-01-24 13:35:18 +0100

    tentative show popup

--- src/components/links/social-media.js
+++ src/components/links/social-media.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function SocialMedia({links}) {
-  return (
-    <div className="social-media">
-    </div>
-  );
+import GlobalImage from "../../shared/components/global-image.js";
+import GlobalPopUp from "../../shared/components/global-popup.js";
+
+export default class SocialMedia extends Component {
+  links;
+  popupMsg;
+  popupHandler;
+  popupIsVisibile = false;
+  hidePopup = () => this.popupIsVisibile = false;
+  showPopup = (linkName, link) => {
+    this.popupIsVisibile = true;
+    this.popupMsg = `We will redirect you to ${linkName} website, confirm?`;
+    this.popupHandler = window.open(link, '_blank');;
+  };
+  
+  constructor() {
+    super();
+  };
+
+  render() {
+    return (
+      <div className="social-media">
+        {
+          this.props.links.map(l =>
+            (
+              <GlobalImage
+                onClickEmitter={ this.showPopup(l.name, l.link) }
+                src={ l.ico }
+                alt={ l.name }>
+              </GlobalImage>
+            )
+          )
+        }
+        {
+          this.popupIsVisibile && (
+            <GlobalPopUp
+              popupMsg={ this.popupMsg }
+              closeEmitter={ this.hidePopup }
+              ctaEmitter={ this.popupHandler }>
+            </GlobalPopUp>
+          )
+        }
+      </div>
+    );
+  }
 }
--- src/shared/components/global-image.js
+++ src/shared/components/global-image.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Image({onClickEmitter, src, alt="Image", width="24", height="24"}) {
+export default function GlobalImage({onClickEmitter, src, alt="Image", width="24", height="24"}) {
   return (
     <div className="shared-image" onClick={ onClickEmitter }>
       <img src={src} alt={alt} width={width} height={height}></img>
